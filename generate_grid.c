b7b072a4d0ff12e206623a84c49519c8ead31c34
Tristan Payen <s.nos.ky@hotmail.fr>
2015-02-27 20:33:54 +0100

    Debut

---
+++ generate_grid.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   generate_grid.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpayen <tpayen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/27 20:26:40 by tpayen            #+#    #+#             */
+/*   Updated: 2015/02/27 20:33:38 by tpayen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "2048.h"
+
+int		*generate_grid(int grid_size)
+{
+	int	*grid;
+
+	if (!(grid = (int *)malloc(sizeof(int) * (grid_size + 1))))
+		return (0);
+	new_number(*grid); /* new_number ajoute un 2 ou 4 dans une case vide */
+	new_number(*grid);
+	return (grid);
+}
