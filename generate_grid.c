1c710e56e11fe90272ac3043b3ee34bae5fde0ef
Tristan Payen <s.nos.ky@hotmail.fr>
2015-02-28 12:16:10 +0100

    Ajout param new_number

--- generate_grid.c
+++ generate_grid.c
@@ -6,7 +6,7 @@
 /*   By: tpayen <tpayen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 20:26:40 by tpayen            #+#    #+#             */
-/*   Updated: 2015/02/27 20:33:38 by tpayen           ###   ########.fr       */
+/*   Updated: 2015/02/28 12:15:45 by tpayen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		*generate_grid(int grid_size)
 
 	if (!(grid = (int *)malloc(sizeof(int) * (grid_size + 1))))
 		return (0);
-	new_number(*grid); /* new_number ajoute un 2 ou 4 dans une case vide */
-	new_number(*grid);
+	new_number(&grid, grid_size); /* new_number ajoute un 2 ou 4 dans une case vide */
+	new_number(&grid, grid_size);
 	return (grid);
 }
